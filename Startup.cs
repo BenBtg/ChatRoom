3ac24af48aa79c82e63d568f9e98f9f6fd3beb89
Ben Buttigieg <bebuttig@microsoft.com>
2018-11-16 11:56:34 +0000

    WebChat basic working

--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,7 @@ namespace ChatRoom
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,10 +25,15 @@ namespace ChatRoom
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            app.UseFileServer();
+            /* app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
+            });*/
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<Chat>("/chat");
             });
         }
     }
